1cb384c108ce921b22ba9101cd4dcc84aae29487
김승룡 <kim5303123@gmail.com>
2018-04-02 16:22:14 +0900

    faq 백에서 데이터 받기 가능

---
+++ src/main/java/com/bgk/delivery/service/FAQDto.java
@@ -0,0 +1,39 @@
+package com.bgk.delivery.service;
+
+public class FAQDto {
+	
+	private String faq_no;
+	private String faq_ask;
+	private String faq_answer;
+	private String tabcon;
+	
+	
+	//게터세터
+	public String getFaq_no() {
+		return faq_no;
+	}
+	public void setFaq_no(String faq_no) {
+		this.faq_no = faq_no;
+	}
+	public String getFaq_ask() {
+		return faq_ask;
+	}
+	public void setFaq_ask(String faq_ask) {
+		this.faq_ask = faq_ask;
+	}
+	public String getFaq_answer() {
+		return faq_answer;
+	}
+	public void setFaq_answer(String faq_answer) {
+		this.faq_answer = faq_answer;
+	}
+	public String getTabcon() {
+		return tabcon;
+	}
+	public void setTabcon(String tabcon) {
+		this.tabcon = tabcon;
+	}
+
+	
+	
+}
